bd6a3955a815e97742a019a7c6b3f3d95b6d6eb4
Adn1217 <adn1217@hotmail.com>
2021-10-31 14:20:00 -0500

    Cayena CRUD Ciclo 3 Deployed to Heroku

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -1,7 +1,15 @@
 import axios from 'axios'
 
 export default() => {
-  return axios.create({
-    baseURL: 'http://localhost:8081/'
-  })
+  if (process.env.NODE_ENV === 'development') {
+    return axios.create({
+      baseURL: 'http://localhost:8081/'
+    })
+  }
+  if (process.env.NODE_ENV === 'production') {
+    return axios.create({
+      baseURL: 'https://cayena-v1.herokuapp.com/'
+    })
+  }
+  // baseURL: process.env.VUE_APP_BASE_URL
 }
