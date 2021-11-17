4942452ca990e9bf4fa1f9812b5016305ea6894f
Adn1217 <adn1217@hotmail.com>
2021-11-16 21:28:00 -0500

    Imagen Cayena

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <!--<img src="./assets/logo.png">-->
+    <img src="./assets/cayena.jpg">
     <router-view/>
   </div>
 </template>
